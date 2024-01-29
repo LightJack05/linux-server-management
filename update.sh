7c922c65257f4be8bf9c7c1a2d13fd85fecd4eaf
LightJack05 <66321084+LightJack05@users.noreply.github.com>
2024-01-29 20:23:41 +0100

    Fixed recursively deleting /

--- update.sh
+++ update.sh
@@ -1,14 +1,18 @@
-# Script for self-updating the management
+# Script for self-updating the management (May also rm -rf your server...)
 
 source variables.sh
+oldInstallRoot=$installRoot
+oldLogFile=$logfile
+oldCronTag=$cronTag
+oldTime=$TIME
 
-echo "[$TIME | Management Update ] Starting management update..." >> $logfile
-echo "[$TIME | Management Update ] Removing old installation..." >> $logfile
+echo "[$oldTime | Management Update ] Starting management update..." >> $oldLogFile
+echo "[$oldTime | Management Update ] Removing old installation..." >> $oldLogFile
 
-rm -rf $installRoot/*
-(crontab -l 2>/dev/null | grep -v "$cronTag") | crontab -
+rm -rf $oldInstallRoot/*
+(crontab -l 2>/dev/null | grep -v "$oldCronTag") | crontab -
 
-echo "[$TIME | Management Update ] Downloading and installing new version..." >> $logfile
+echo "[$oldTime | Management Update ] Downloading and installing new version..." >> $oldLogFile
 
-curl $repoUrl/install.sh | bash | sed -e "s/^/[$TIME | Management Update] /" >> $logfile
+curl $repoUrl/install.sh | bash | sed -e "s/^/[$oldTime | Management Update] /" >> $oldLogFile
 
