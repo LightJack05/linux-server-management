f46dc2a17cb12342b0d57cf57a8a27598fc96eec
LightJack05 <66321084+LightJack05@users.noreply.github.com>
2024-01-29 19:33:45 +0100

    Renamed update to update-server

--- weekly.sh
+++ weekly.sh
@@ -3,4 +3,4 @@
 # This script is intended to call other scripts for organization. Please create jobs in other scripts and call them from here!
 source variables.sh
 
-$installRoot/update.sh
+$installRoot/update-server.sh
